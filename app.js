7bbb38bfcec128ddb0f386bd6f5af04f377dc7b5
KaitoVux <39855103+KaitoVux@users.noreply.github.com>
2020-10-03 16:39:20 +0700

    fix time counter

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const yourDate = new Date("2020-08-22T00:00:00"),
       music = ["I Do - 911", "The One - Kodaline", "All Of Me - John Legend", "Make You Feel My Love - Adele", "Wake Me Up When September Ends - Green D", "Can't Take My Eyes Off You", "Say You Won't Let Go - James Arthur"];
 let isPlayed = [];
 
+console.log();
+
 document.addEventListener('DOMContentLoaded', function () {
       var rootTime = document.querySelector("time");
 
       document.querySelector("anni").textContent = `${(yourDate.getDate() > 9) ? yourDate.getDate() : "0" + yourDate.getDate()}-${(yourDate.getMonth() > 8) ? (yourDate.getMonth() + 1) : "0" + (yourDate.getMonth() + 1)}-${yourDate.getFullYear()}`;
 
-      document.querySelector("date").textContent = Math.floor(Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24) + " days";
+      document.querySelector("date").textContent = Math.floor((new Date().getTime() - yourDate.getTime()) / 1000 / 60 / 60 / 24) + " days";
 
       function olock() {
             var today = new Date(),
-                  hrs = (Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
-                  min = (Math.floor(Math.floor((today - yourDate) / 1000) / 60)) % 60,
-                  sec = Math.floor((today - yourDate) / 1000) % 60;
+                  hrs = today.getHours();
+            min = today.getMinutes();
+            sec = today.getSeconds();
             rootTime.textContent = `${(hrs > 9) ? hrs : "0" + hrs}:${(min > 9) ? min : "0" + min}:${(sec > 9) ? sec : "0" + sec}`;
       } olock();
       var timer = setInterval(function () { olock() }, 1000);
