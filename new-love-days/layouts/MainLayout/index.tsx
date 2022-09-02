8d853a4436415ca08d93b24450a072f8fc06b78c
Dat Vu <dat.vt@zodinet.com>
2022-09-02 12:52:01 +0700

    feat: migrate to next

--- new-love-days/layouts/MainLayout/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC, PropsWithChildren } from "react";
-
-export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
-    return <main className="container mx-auto">{children}</main>;
-};
